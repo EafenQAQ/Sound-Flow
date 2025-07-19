31757651e2c4e76353b2a9fcc85a4011c72c0d9e
EafenQAQ <eafenqaq@gmail.com>
2025-07-19 14:32:29 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="content">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+import { RouterView } from 'vue-router';
+
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="HomeView">
+    HomePage
+    <p>这是一串中文字符串</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
