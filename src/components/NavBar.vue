4b08ef79f5acc1592830d507993d468b1e24c4fd
EafenQAQ <eafenqaq@gmail.com>
2025-07-20 15:41:24 +0800

    feat：登出功能

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -10,16 +10,38 @@
     <div class="btn-container">
       <RouterLink :to="{ name: 'login' }"><button>登录</button></RouterLink>
       <RouterLink :to="{ name: 'signup' }"><button>注册</button></RouterLink>
-      <RouterLink :to="{ name: 'home' }"><button>登出</button></RouterLink>
 
-
-
+      <div class="logout-group">
+        <button @click="handleLogout">登出</button>
+        <Transition name="fade" mode="out-in">
+          <p v-if="success" class="success">{{ success }}</p>
+        </Transition>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import { useLogout } from '@/composables/useLogout';
+import { ref } from 'vue';
+import { RouterLink, useRouter } from 'vue-router';
+
+const { error, logout } = useLogout()
+const success = ref(null)
+const router = useRouter()
+
+const handleLogout = async () => {
+  await logout()
+  if (!error.value) {
+    success.value = '登出成功！'
+    setTimeout(() => {
+      success.value = null
+      router.push({ name: 'home' })
+
+    }, 2000
+    )
+  }
+}
 
 
 </script>
@@ -51,5 +73,29 @@ import { RouterLink } from 'vue-router';
 
 img {
   border-radius: 50%;
+}
+
+
+
+.logout-group {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: relative;
+  height: 100%;
+}
+
+.success {
+  /* 让success标语出现在logout键下面 */
+  position: absolute;
+  bottom: 0%;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: 0.9rem;
+  width: 100%;
+
+
+
+
 }
 </style>
--- src/composables/useLogout.js
+++ src/composables/useLogout.js
@@ -10,13 +10,12 @@ const logout = async () => {
     error.value = null
   } catch (err) {
     console.error(err.message)
-    error.value = err.message
+    error.value = '用户登出失败'
   }
 }
 
-
 const useLogout = () => {
-  return {error, logout}
+  return { error, logout }
 }
 
-export {useLogout}
+export { useLogout }
