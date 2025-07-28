b03f0c66b64480a26922c7b39a3632b8c281d4af
EafenQAQ <eafenqaq@gmail.com>
2025-07-28 22:54:58 +0800

    perf: 引入rollup-plugin-visualizer并手动配置chunks，将过大的firebase依赖从主包中分离

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), visualizer({ open: true, filename: 'bundle-report.html' })],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        // 在这里添加对chunks的配置
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // 将vue相关的库分到一起
+            if (id.includes('vue') || id.includes('pinia') || id.includes('vue-router')) {
+              return 'vendor-vue'
+            }
+
+            // firebase自己一桌
+            if (id.includes('firebase')) {
+              return 'vendor-firebase'
+            }
+            // 其他
+            return 'vendor'
+          }
+        },
+      },
     },
   },
 })
